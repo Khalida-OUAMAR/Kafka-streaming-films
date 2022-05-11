8cde5e677ae997ccd95367d5d59db732f4e29b54
alexdav2b <alexdav2b@gmail.com>
2022-05-11 14:36:13 +0200

    Full front

---
+++ Website/front/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.main_page, name='main page'),
+    path('get/ajax/list', views.get_list, name="get_list"),
+
+    # path('', views.main_page, name='main page'),
+    # path('get/ajax/lyrics', views.get_lyrics, name="get_lyrics"),
+    # path('get/ajax/parody_lyrics', views.get_parody, name="get_parody")
+]
