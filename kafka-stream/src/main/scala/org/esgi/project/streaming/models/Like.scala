d7101f65e5a31d0601ee56898cedd9a80c95ecc1
OUAMAR KHALIDA <khalida.ouamar@etu.unistra.fr>
2022-05-11 10:21:29 +0200

    create View and Like classes

---
+++ kafka-stream/src/main/scala/org/esgi/project/streaming/models/Like.scala
@@ -0,0 +1,11 @@
+package org.esgi.project.streaming.models
+
+import play.api.libs.json.{Json, OFormat}
+
+case class Like(
+                 _id: String,
+                 score: Long)
+
+object Like {
+  implicit val format: OFormat[Like] = Json.format[Like]
+}
